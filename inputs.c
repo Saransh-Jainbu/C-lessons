4fd3cae13d205600d7b37aedb0b0c29735d23925
Saransh Jain <saranshj647@gmail.com>
2024-02-04 10:36:48 +0530

    learned how to take inputs

---
+++ inputs.c
@@ -0,0 +1,10 @@
+//In this module we will learn how to take inputs in c
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("Enter the value of a,b: ");
+    scanf("%d  %d",&a,&b);//taking two integer input from user and storing them
+    printf("%d  %d\n",a,b);
+
+
+}
